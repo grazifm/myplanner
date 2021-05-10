06770fb1652f81f03136aea1098169166066225d
Graziela França <36572179+grazifm@users.noreply.github.com>
2021-05-10 16:49:15 +0930

    Add files via upload
    Controller e classe de inicialização

---
+++ MyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.myplanner.myplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplannerApplication.class, args);
+	}
+
+}
